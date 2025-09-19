b9db96919c962bf1402de540994b5f92965e7c63
SabbirHsarkar <sabbirhossainsarkar575@gmail.com>
2025-09-20 01:36:11 +0600

    Some Updated

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Country.css'
+
+const Country = ({country}) => {
+    // const countryData=use(country);
+    console.log(country);
+
+    const handleVisited=()=>{
+        console.log("Clicked");
+    }
+    return (
+       
+            
+            <div className='country'>
+ <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h3>Name: {country.name.common}</h3>
+            <p>Population: {country.population.population}</p>
+            <p>Region: {country.region.region}</p>
+
+            <p>Area: {country.area.area} {
+                
+                country.area.area >300000 ? "-Big Country":"-Small Country"
+                }</p>
+         <button onClick={handleVisited}>Not visited</button>
+
+            </div>
+           
+       
+    );
+};
+
+export default Country;
